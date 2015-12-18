bbd92ebeddc093ce0b8688da8dd5a4239b14098f
Vasiliy Zaycev <bazeel.z@gmail.com>
2015-12-18 17:33:03 +0300

    get messages list

--- djrest/notification/views.py
+++ djrest/notification/views.py
@@ -8,7 +8,6 @@ from .serializer import MessageSerializer
 class MessageList(APIView):
 
     def get(self, request, format=None):
-        print request.user.id
         messages = Message.objects.filter(user__id=request.user.id, success=True).order_by('-date_sent')
         serializer = MessageSerializer(messages, many=True)
         return Response(serializer.data)
