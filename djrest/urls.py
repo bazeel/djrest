a812b89950f69f1b93118a5502a7950a448c09e1
Vasiliy Zaycev <bazeel.z@gmail.com>
2015-12-18 16:09:44 +0300

    rest framework authentication token

--- djrest/urls.py
+++ djrest/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework import routers
+from rest_framework.authtoken import views
 
 
 # Routers provide an easy way of automatically determining the URL conf.
@@ -25,5 +26,6 @@ router = routers.DefaultRouter()
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', views.obtain_auth_token),
 ]
