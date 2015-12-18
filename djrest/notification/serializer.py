35203281ffefd4297760136dc220095dd413e361
Vasiliy Zaycev <bazeel.z@gmail.com>
2015-12-18 17:30:08 +0300

    get messages list

---
+++ djrest/notification/serializer.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Message
+
+
+class MessageSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(read_only=True)
+    text = serializers.CharField(read_only=True)
+    date_sent = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Message` instance, given the validated data.
+        """
+        return Message.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        """
+        return instance
--- djrest/notification/views.py
+++ djrest/notification/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Message
+from .serializer import MessageSerializer
 
-# Create your views here.
+
+class MessageList(APIView):
+
+    def get(self, request, format=None):
+        print request.user.id
+        messages = Message.objects.filter(user__id=request.user.id, success=True).order_by('-date_sent')
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
