6509ad113c3cb3be1d74942e76c9884e20cc6bc5
Vasiliy Zaycev <bazeel.z@gmail.com>
2015-12-21 12:48:58 +0300

    pep8 line too long

--- djrest/notification/views.py
+++ djrest/notification/views.py
@@ -8,6 +8,7 @@ from .serializer import MessageLogSerializer
 class MessageLogList(APIView):
 
     def get(self, request, format=None):
-        messages = MessageLog.objects.filter(user__id=request.user.id, success=True).order_by('-date_sent', '-date_created')
+        messages = MessageLog.objects.filter(user__id=request.user.id, success=True).order_by('-date_sent',
+                                                                                              '-date_created')
         serializer = MessageLogSerializer(messages, many=True)
         return Response(serializer.data)
